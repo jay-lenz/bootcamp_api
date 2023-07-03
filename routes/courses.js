0af302c4fe0b3e4a6a94c0dd91b792e816c6bb97
Jenyo Oyebode <jenyooyebode@icloud.com>
2023-07-03 10:58:52 -0400

    Create courses.js

---
+++ routes/courses.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const router = express.Router({ mergeParams: true })
+
+const { getCourses, getCourse, addCourse, updateCourse, deleteCourse } = require('../controllers/courses')
+
+const Course = require('../models/Course')
+const advancedResults = require('../middleware/advancedResult')
+const { protect, authorize } = require('../middleware/auth')
+
+router.route('/')
+    .post(protect, authorize('publisher', 'admin'), addCourse)
+    .get(advancedResults(Course, {
+        path: 'bootcamp',
+        select: 'name description housing'
+    }), getCourses)
+
+router.route('/:id').get(getCourse)
+    .put(protect, authorize('publisher', 'admin'), updateCourse)
+    .delete(protect, authorize('publisher', 'admin'), deleteCourse)
+module.exports = router
+
+
+
